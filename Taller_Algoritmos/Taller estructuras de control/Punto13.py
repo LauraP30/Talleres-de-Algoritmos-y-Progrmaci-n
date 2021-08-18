395b7873811658d70ff3ac448362c7d17c6aa98d
Laura Pérez <87994943+LauraP30@users.noreply.github.com>
2021-08-18 16:53:33 -0500

    Update Punto13.py

---
+++ Taller_Algoritmos/Taller estructuras de control/Punto13.py
@@ -0,0 +1,16 @@
+"""
+Salidas
+Total-->int-->T
+"""
+#caja negra
+a=1*50000
+b=2*20000
+c=3*10000
+d=4*5000
+e=5*2000
+f=6*1000
+g=7*500
+h=8*100
+T=a+b+c+d+e+f+g+h
+#Salidas
+print("Dinero total del banco es: $"+str(T))
