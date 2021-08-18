cbc459bd242ee202af168da3dd55c9595b3b13c9
Laura Pérez <87994943+LauraP30@users.noreply.github.com>
2021-08-18 16:48:41 -0500

    Update Punto2.py

---
+++ Taller_Algoritmos/Taller estructuras de control/Punto2.py
@@ -0,0 +1,12 @@
+”””
+Entradas
+Capital-->int-->a
+Salidas
+Ganancia-->int-->b
+"""
+a=int(input("Digite el capital: "))
+#caja negra
+b=a*0.2
+#Salidas
+print("Su ganancia es: $","{:.0f}".format(b))
+
