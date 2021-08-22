0d3249e4a0e5c63dc6a4468f8356242d10b3ba26
Laura Pérez <87994943+LauraP30@users.noreply.github.com>
2021-08-22 16:19:25 -0500

    Ejercicio8
    Subí el ejercicio 8 del taller.

---
+++ Taller_Algoritmos/Taller Estructuras de Control Selectivas/Ejercicio8.py
@@ -0,0 +1,11 @@
+"""
+Entradas
+p-->int-->P
+q-->int-->Q
+"""
+P=int(input())
+Q=int(input())
+if(P**3+Q**4-2*P**2>680):
+  print("P y Q satisfacen la expresion")
+else:
+  print("P y Q no satisfacen la expresion")
